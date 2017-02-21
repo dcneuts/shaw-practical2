b576bd2b4e22b8ed17c2d5198eb9114076e7c4e8
Derek Neuts <dcneuts@gmail.com>
2017-02-20 22:56:46 -0800

    Reworked index.html and added countdown.php as a temporary solutions, despite non-formal PHP format for this lesson.

---
+++ countdown.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Shaw Academy Practical Countdown Exercise</title>
+</head>
+<body>
+<p id="hours"></p>
+<p id="minutes"></p>
+<p id="seconds"></p>
+
+<script>
+    var start = new Date(2017, 2, 20, 23); //fix this to current year mo date hours
+
+    updateCountdown("hours","minutes","seconds");
+    setInterval(function(){updateCountdown("hours","minutes","seconds")}, 1000);
+
+    function updateCountdown(hoursId,minutesId,secondsId) {
+        var count = timeRemaining();
+        document.getElementById(hoursId).innerHTML = count.hours;
+        document.getElementById(minutesId).innerHTML = count.minutes;
+        document.getElementById(secondsId).innerHTML = count.seconds;
+    }
+
+    function timeRemaining() {
+        var now = new Date();
+        var timeLeft = start.getTime() - now.getTime(); //in milliseconds
+        var hours = timeLeft / (1000 * 60 * 60);
+        hours = Math.floor(hours);
+        var minutes = timeLeft / (1000 * 60) - hours * 60;
+        minutes = Math.floor(minutes);
+        var seconds = timeLeft / (1000) - hours * 60 * 60 - minutes * 60;
+        seconds = Math.floor(seconds);
+        return {hours: hours, minutes: minutes, seconds: seconds};
+    }
+
+</script>
+</body>
+</html>
